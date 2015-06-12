9c552591ddf21042f7b0904a906e99ff351749e6
paigesaez <paigesaez@gmail.com>
2015-06-12 11:23:10 -0500

    more fussing with gh-pages

--- js/pulseband.js
+++ js/pulseband.js
@@ -6,3 +6,7 @@ $('body').scrollspy({ target: '#navbar-header' });
 
 //navigation starts @ {bottom:30px, navbar-fixed-bottom}
 //navigation ends @ {bottom:30px, navbar-fixed-top}
+
+
+
+//navigation locks to top at the point where we scroll to features section
